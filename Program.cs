7ebbf95919792a6d774397653fe2e7dd35680c59
vlad <vlad19972017@gmail.com>
2022-12-01 21:31:19 +0200

    123

---
+++ Program.cs
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------
+// <copyright file="NameOfFile.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            // Your lab.
+        }
+    }
+}
