38c4f35663b6200203c360003e66a03756bbfa1c
vlad <vlad19972017@gmail.com>
2022-12-01 21:11:55 +0200

    Add lab 4.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------
+// <copyright file="NameOfFile.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your lab.
+        }
+    }
+}
